31dfa092895ec37048fbe74766095658b216e947
Julian <victorj19@gmail.com>
2026-03-19 17:06:21 -0500

    test terraform CI-3

---
+++ terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "921292479940-julian-c"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
